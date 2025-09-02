6e6303662b20832a262bc6c7dc1ee74d2662a285
jarnoriu <jarno.riuttala@metropolia.fi>
2025-09-02 19:19:32 +0300

    Tehtävän palautus

--- Tehtävät/Tehtava_2.5.py
+++ Tehtävät/Tehtava_2.5.py
@@ -3,5 +3,11 @@ a= float(input("Anna leiviskät:"))
 b= float(input("Anna naulat:"))
 c= float(input("Anna luodit:"))
 #Laskenta ja tulostus
-
-print(f"Massa nykymittojen mukaan:", str(((a*20*32*13.3)+(b*32*13.3)+(c*13.3))),"grammaa")
+#laskenta
+paino=float((a*20*32*13.3)+(b*32*13.3)+(c*13.3))
+#kilogramma ja gramma laskenta
+kg=int(paino/1000)
+grammat=float(paino-(kg*1000))
+#Tulostus ja desimaali rajaus
+print(f"{kg}kilogrammaa ja {grammat:.2f}grammaa")
+#print(f"Massa nykymittojen mukaan:", str(((a*20*32*13.3)+(b*32*13.3)+(c*13.3))),"grammaa")
